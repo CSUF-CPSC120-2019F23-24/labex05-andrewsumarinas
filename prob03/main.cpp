5e3f42023bf9da4ac9cb47a5279897b85aaa38c2
ALSumarinas <alsumarinas@cs104u-4c.acad.fullerton.edu>
2019-09-21 14:38:15 -0700

    Prob03-AndrewSumarinas

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,30 @@
 // Random Rectangle
+
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+
+int main()
+{
+  //I planted a seed, so everytime, the code executes, it will randomly
+  //generate a number everytime the program is executed.
+  unsigned seed = time(0);
+  srand(seed);
+  std::string numSign;
+  int max = 80;   //maximum value for the length of the rectangle
+  int myNum;
+
+  myNum = rand() % max + 1;
+
+  numSign.assign(myNum, '#'); //here is where I assign the random number to the string
+
+  std::cout << numSign << std::endl;
+  std::cout << numSign << std::endl;
+  std::cout << numSign << std::endl;
+
+
+  return 0;
+
+}
