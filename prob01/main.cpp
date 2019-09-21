be77691e9269a68b078a3cb73e6496281377509c
ALSumarinas <alsumarinas@cs104u-4c.acad.fullerton.edu>
2019-09-21 13:24:04 -0700

    Prob01-AndrewSumarinas

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -27,24 +27,30 @@ int main()
   d2 *= 0.01;
   f2 *= 0.01;
 
+  std::cout << std::endl;
   // TODO#1 add comment describing what's happening
-  std::cout << "\n**comment**" << std::endl;
+  std::cout << "Both values begin to become smaller as they get closer to 0" << std::endl;
+  std::cout << "But since they have different data types, the value for float is smaller" << std::endl;
   std::cout << sizeof(d2) << " bytes, value: " << d2 << std::endl;
   std::cout << sizeof(f2) << " bytes, value: " << f2 << std::endl;
 
+
   d2 *= 0.1;
   f2 *= 0.1;
+  std::cout << std::endl;
 
   // TODO#2 add comment describing what's happening
-  std::cout << "\n**comment**" << std::endl;
+  std::cout << "As we multiply both numbers by a constant, both values decrease, with the float value being smaller ";
+  std::cout << "and closer to 0" << std::endl;
   std::cout << sizeof(d2) << " bytes, value: " << d2 << std::endl;
   std::cout << sizeof(f2) << " bytes, value: " << f2 << std::endl;
 
   double d3 = 3.5E38;
   float f3 = 3.5E38;
 
+  std::cout << std::endl;
   // TODO#3 add comment describing what happened
-  std::cout << "\n**comment**" << std::endl;
+  std::cout << "Since the maximum value for float is up to 38, it is showing as \"inf\"" << std::endl;
   std::cout << std::setprecision(1);
   std::cout << sizeof(d3) << " bytes, value: " << d3 << std::endl;
   std::cout << sizeof(f2) << " bytes, value: " << f3 << std::endl;
